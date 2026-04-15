a07ee9459daa601c80c25e46f23f6163cdd71aa6
Himanshu Shekhar <himanshuji0211@gmail.com>
2026-04-15 21:39:04 +0530

    Add left rotation of an array by d steps

---
+++ src/arrays/LeftRotation.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Arrays;
+
+//Problem: Common Elements
+//Platform: GeeksforGeeks
+//Problem Link : https://www.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1
+//Difficulty: Medium
+//Topics: Arrays
+//Time Complexity: O(n)
+//Space Complexity: O(1) 
+
+class LeftRotation {
+	public static void reverse(int arr[], int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void rotateArr(int arr[], int d) {
+        int n = arr.length;
+       d = d%n;
+       reverse(arr, 0, d-1);
+       reverse(arr, d, n-1);
+       reverse(arr, 0, n-1);
+    }
+	public static void main(String[] args) {
+		int arr [] = {1,2,3,4,5};
+		rotateArr(arr, 2);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
